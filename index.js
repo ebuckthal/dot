b852b19a6707eb50dc832a318c47f8197fb427c4
Eric Buckthal <eric.buckthal@vsclimbing.com>
2021-10-10 17:44:45 -0400

    Clean up functionality

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * https://discord.com/api/oauth2/authorize?client_id=895802049768353832&permissions=8&scope=bot%20messages.read
+ */
+
 const { Client, Intents } = require("discord.js");
 const config = require("./config.json");
 const fetch = require("isomorphic-unfetch");
@@ -10,32 +14,71 @@ const client = new Client({
   ],
 });
 
-// https://discord.com/api/oauth2/authorize?client_id=895802049768353832&permissions=8&scope=bot%20messages.read
-
-let data = [];
-let offset = 0;
-
+// translates strange xml-shaped data into list of socres, the maximum second
+// represented in those scores and the offset between "serverTime" and our
+// clock
 const parseScore = (text) => {
   var p = /<serverTime>([\d.]+)/im,
     pi = /<s t='([\d]+)'>([\d.]+)/gim,
     r;
 
-  var nextData = [];
-  var serverTime = p.exec(text)[1];
+  const scores = new Map();
+  const serverTime = p.exec(text)[1];
+  let maxSecond;
 
   do {
     if ((r = pi.exec(text))) {
-      nextData[r[1]] = parseFloat(r[2]);
+      maxSecond = parseInt(r[1], 10);
+      scores.set(parseInt([r[1]], 10), parseFloat(r[2]));
     }
   } while (r);
 
   const currentSecond = Math.floor(Date.now() / 1000.0);
   offset = currentSecond - serverTime;
 
-  data = nextData;
+  return { scores, maxSecond, offset };
 };
 
-const getDot = (score) => {
+const fetchData = async () => {
+  const f = await fetch(
+    `https://global-mind.org/gcpdot/gcpindex.php?current=1&nonce=${Math.round(
+      Math.random() * 10000000
+    )}`
+  );
+  return parseScore(await f.text());
+};
+
+// Score generator keeps track of state and determines whether or not we have
+// the current score already fetched or if we need to fetch again.
+function initScoreGenerator() {
+  let maxSecond = 0;
+  let offset = 0;
+  let scores = new Map();
+
+  const refetchData = async () => {
+    console.log("[FETCH]");
+    const gcpData = await fetchData();
+    maxSecond = gcpData.maxSecond;
+    offset = gcpData.offset;
+    scores = gcpData.scores;
+    console.log("[FETCH]", { maxSecond });
+  };
+
+  // checks if we have data (currentSecond >= maxSecond). If not, refetches
+  // from server. Then, returns the score at the currentSecond;
+  return async () => {
+    currentSecond = Math.floor(Date.now() / 1000.0) - offset;
+    console.info("[GET SCORE]", { currentSecond, maxSecond });
+    if (currentSecond >= maxSecond) {
+      await refetchData();
+      currentSecond = Math.floor(Date.now() / 1000.0) - offset;
+    }
+    console.info("[GET SCORE]", { score: scores.get(currentSecond) });
+    return scores.get(currentSecond);
+  };
+}
+
+const scoreToDot = (score) => {
   if (score < 0.05) {
     return "🔴";
   } else if (score < 0.1) {
@@ -53,34 +96,16 @@ const getDot = (score) => {
   }
 };
 
-const setData = async () => {
-  console.log("[FETCH]");
-  const f = await fetch(
-    `https://global-mind.org/gcpdot/gcpindex.php?current=1&nonce=${Math.round(
-      Math.random() * 10000000
-    )}`
-  );
-  parseScore(await f.text());
-};
-
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag} !`);
-
-  setData();
-  setInterval(async () => {
-    setData();
-  }, [20000]);
+  const scoreGen = initScoreGenerator();
 
   setInterval(async () => {
-    const currentSecond = Math.floor(Date.now() / 1000.0) - offset;
-    const score = data[currentSecond];
-    console.log("[UPDATE] ", { currentSecond, score });
-    if (score) {
-      const r = client.user.setActivity({
-        type: "WATCHING",
-        name: `${getDot(score)}${score.toFixed(4)}`,
-      });
-    }
+    const score = await scoreGen();
+    const r = client.user.setActivity({
+      type: "WATCHING",
+      name: `${scoreToDot(score)}${score.toFixed(4)}`,
+    });
   }, [5000]);
 });
 
